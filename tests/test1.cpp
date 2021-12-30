d469c2a1f6a3b24fdbb9194af814eca106c7f07f
cursey <cursey@live.com>
2021-12-30 10:14:59 -0800

    Improve the example usage

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 
 #include <SafetyHook.hpp>
@@ -16,12 +15,19 @@ int hook_add(int x, int y) {
 int main(int argc, char* argv[]) {
     std::cout << "unhooked add(2, 3) = " << add(2, 3) << "\n";
 
-    auto factory = SafetyHookFactory::init();
-
     {
-        // Builder keeps all threads frozen until it goes out of scope.
+        // Create a factory that will create hooks for us.
+        auto factory = SafetyHookFactory::init();
+
+        // Acquire the factories builder which will freeze all threads and give
+        // us access to the hook creation methods.
         auto builder = factory->acquire();
+
+        // Create a hook on add
         g_add_hook = builder.create(add, hook_add);
+
+        // Once we leave this scope, builder will unfreeze all threads and our
+        // factory will be kept alive by g_add_hook.
     }
 
     std::cout << "hooked add(2, 3) = " << add(2, 3) << "\n";
