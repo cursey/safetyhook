d729101e566874e20d6143c06374a361278e60c2
cursey <cursey@live.com>
2021-12-29 17:01:36 -0800

    Use E9 jmps where appropriate and do not follow existing jmps

--- src/SafetyHook.cpp
+++ src/SafetyHook.cpp
@@ -26,34 +26,61 @@ private:
 };
 
 #include <pshpack1.h>
-struct Jmp {
-    uint8_t prefix{0xFF};
-    uint8_t opcode{0x25};
+struct JmpE9 {
+    uint8_t opcode{0xE9};
+    uint32_t offset{0};
+};
+
+struct JmpFF {
+    uint8_t opcode0{0xFF};
+    uint8_t opcode1{0x25};
     uint32_t offset{0};
 };
 #include <poppack.h>
 
-constexpr auto make_jmp(uintptr_t src, uintptr_t dst, uintptr_t data) {
-    Jmp jmp{};
+constexpr auto make_jmp_ff(uintptr_t src, uintptr_t dst, uintptr_t data) {
+    JmpFF jmp{};
 
-    jmp.offset = data - src - 6;
+    jmp.offset = data - src - sizeof(jmp);
     *(uintptr_t*)data = dst;
 
     return jmp;
 }
 
-static void emit_jmp(uintptr_t src, uintptr_t dst, uintptr_t data, size_t size = sizeof(Jmp)) {
-    if (size < sizeof(Jmp)) {
+static void emit_jmp_ff(uintptr_t src, uintptr_t dst, uintptr_t data, size_t size = sizeof(JmpFF)) {
+    if (size < sizeof(JmpFF)) {
         return;
     }
 
     UnprotectMemory _{src, size};
 
-    if (size > sizeof(Jmp)) {
+    if (size > sizeof(JmpFF)) {
         std::fill_n((uint8_t*)src, size, 0x90);
     }
 
-    *(Jmp*)src = make_jmp(src, dst, data);
+    *(JmpFF*)src = make_jmp_ff(src, dst, data);
+}
+
+constexpr auto make_jmp_e9(uintptr_t src, uintptr_t dst) {
+    JmpE9 jmp{};
+
+    jmp.offset = dst - src - sizeof(jmp);
+
+    return jmp;
+}
+
+constexpr void emit_jmp_e9(uintptr_t src, uintptr_t dst, size_t size = sizeof(JmpE9)) {
+    if (size < sizeof(JmpE9)) {
+        return;
+    }
+
+    UnprotectMemory _{src, size};
+
+    if (size > sizeof(JmpE9)) {
+        std::fill_n((uint8_t*)src, size, 0x90);
+    }
+
+    *(JmpE9*)src = make_jmp_e9(src, dst);
 }
 
 constexpr auto follow_offset_at(uintptr_t address) {
@@ -103,14 +130,14 @@ static uintptr_t follow_jmps(uintptr_t ip) {
 SafetyHook::SafetyHook(std::shared_ptr<SafetyHookFactory> manager, uintptr_t target, uintptr_t destination)
     : m_manager{manager} {
     ThreadFreezer threads{};
-    m_target = follow_jmps(target);
-    m_destination = follow_jmps(destination);
+    m_target = target; // follow_jmps(target);
+    m_destination = destination; // follow_jmps(destination);
     auto ip = m_target;
     std::vector<uintptr_t> desired_addresses{};
 
     desired_addresses.emplace_back(m_target);
 
-    while (m_trampoline_size < sizeof(Jmp)) {
+    while (m_trampoline_size < sizeof(JmpE9)) {
         INSTRUX ix{};
 
         if (!decode(&ix, ip)) {
@@ -120,13 +147,16 @@ SafetyHook::SafetyHook(std::shared_ptr<SafetyHookFactory> manager, uintptr_t tar
         if (ix.HasDisp && ix.DispLength == 4) {
             auto target_address = ip + ix.Length + (int32_t)ix.Displacement;
             desired_addresses.emplace_back(target_address);
+        } else if (ix.HasRelOffs && ix.RelOffsLength == 4) {
+            auto target_address = ip + ix.Length + (int32_t)ix.RelativeOffset;
+            desired_addresses.emplace_back(target_address);
         }
 
         m_trampoline_size += ix.Length;
         ip += ix.Length;
     }
 
-    m_trampoline_allocation_size = m_trampoline_size + sizeof(Jmp) + sizeof(uintptr_t) * 2;
+    m_trampoline_allocation_size = m_trampoline_size + sizeof(JmpE9) + sizeof(JmpFF) + sizeof(uintptr_t);
     m_trampoline = m_manager->allocate_near(desired_addresses, m_trampoline_allocation_size);
 
     std::copy_n((const uint8_t*)m_target, m_trampoline_size, std::back_inserter(m_original_bytes));
@@ -144,14 +174,30 @@ SafetyHook::SafetyHook(std::shared_ptr<SafetyHookFactory> manager, uintptr_t tar
             auto target_address = m_target + i + ix.Length + (int32_t)ix.Displacement;
             auto new_disp = (int32_t)(target_address - (m_trampoline + i + ix.Length));
             *(uint32_t*)(m_trampoline + i + ix.DispOffset) = new_disp;
+        } else if (ix.HasRelOffs && ix.RelOffsLength == 4) {
+            auto target_address = m_target + i + ix.Length + (int32_t)ix.RelativeOffset;
+            auto new_disp = (int32_t)(target_address - (m_trampoline + i + ix.Length));
+            *(uint32_t*)(m_trampoline + i + ix.RelOffsOffset) = new_disp;
         }
 
         i += ix.Length;
     }
 
-    emit_jmp(m_trampoline + m_trampoline_size, ip, m_trampoline + m_trampoline_size + sizeof(Jmp));
-    emit_jmp(m_target, (uintptr_t)m_destination, m_trampoline + m_trampoline_size + sizeof(Jmp) + sizeof(uintptr_t),
-        m_trampoline_size);
+    // jmp from trampoline to original.
+    auto src = m_trampoline + m_trampoline_size;
+    auto dst = ip;
+    emit_jmp_e9(src, dst);
+
+    // jmp from original to trampoline.
+    src = m_target;
+    dst = m_trampoline + m_trampoline_size + sizeof(JmpE9);
+    emit_jmp_e9(src, dst, m_trampoline_size);
+
+    // jmp from trampoline to destination.
+    src = m_trampoline + m_trampoline_size + sizeof(JmpE9);
+    dst = m_destination;
+    auto data = src + sizeof(JmpFF);
+    emit_jmp_ff(src, dst, data);
 
     for (auto i = 0; i < m_trampoline_size; ++i) {
         threads.fix_ip(m_target + i, m_trampoline + i);
