d01e5b48b4d1f314046a9e6b44cd8336ee02d4c7
cursey <cursey@live.com>
2023-03-04 23:22:39 -0800

    ThreadFreezer: Expose as SafetyThreadFreezer in safetyhook.hpp

--- include/safetyhook.hpp
+++ include/safetyhook.hpp
@@ -2,9 +2,11 @@
 
 #include "safetyhook/inline_hook.hpp"
 #include "safetyhook/mid_hook.hpp"
+#include "safetyhook/thread_freezer.hpp"
 
 using SafetyHookContext = safetyhook::Context;
 using SafetyHookInline = safetyhook::InlineHook;
 using SafetyHookMid = safetyhook::MidHook;
 using SafetyInlineHook [[deprecated("Use SafetyHookInline instead.")]] = safetyhook::InlineHook;
 using SafetyMidHook [[deprecated("Use SafetyHookMid instead.")]] = safetyhook::MidHook;
+using SafetyThreadFreezer = safetyhook::ThreadFreezer;
