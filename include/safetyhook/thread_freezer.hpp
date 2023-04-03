360c24ea308b81ce5a46a9212f9267fa80575b33
cursey <cursey@live.com>
2023-04-02 21:03:01 -0700

    ThreadFreezer: Refactor to `execute_while_frozen` This avoid issues with allocating causing a deadlock

--- include/safetyhook/thread_freezer.hpp
+++ include/safetyhook/thread_freezer.hpp
@@ -4,51 +4,24 @@
 #pragma once
 
 #include <cstdint>
+#include <functional>
 #include <vector>
 
 #include <Windows.h>
 
 namespace safetyhook {
+/// @brief Executes a function while all other threads are frozen. Also allows for visiting each frozen thread and
+/// modifying it's context.
+/// @param run_fn The function to run while all other threads are frozen.
+/// @param visit_fn The function that will be called for each frozen thread.
+/// @note The visit function will be called in the order that the threads were frozen.
+/// @note The visit function will be called before the run function.
+/// @note Keep the logic inside run_fn and visit_fn as simple as possible to avoid deadlocks.
+void execute_while_frozen(std::function<void()> run_fn, std::function<void(uint32_t, HANDLE, CONTEXT&)> visit_fn = {});
 
-/// @brief A class for freezing all threads in the process.
-class ThreadFreezer final {
-public:
-    ThreadFreezer(ThreadFreezer&) = delete;
-    ThreadFreezer(ThreadFreezer&&) noexcept = default;
-    ThreadFreezer& operator=(ThreadFreezer&) = delete;
-    ThreadFreezer& operator=(ThreadFreezer&&) noexcept = default;
-
-    /// @brief Constructs a new ThreadFreezer object.
-    /// @note This freezes all threads in the process except the current thread.
-    ThreadFreezer();
-
-    /// @brief Destructs the ThreadFreezer object.
-    /// @note This unfreezes all threads in the process.
-    ~ThreadFreezer();
-
-    /// @brief Fixes any threads that are currently on `old_ip` and sets them to `new_ip`.
-    /// @param old_ip The old instruction pointer.
-    /// @param new_ip The new instruction pointer.
-    void fix_ip(uintptr_t old_ip, uintptr_t new_ip);
-
-private:
-    struct FrozenThread {
-        uint32_t thread_id{};
-        HANDLE handle{};
-        CONTEXT ctx{};
-    };
-
-    // Custom allocator for `m_frozen_threads` that will work while threads are being frozen. This is necessary because
-    // `new` can deadlock while freezing threads.
-    template <class T> struct HeapAllocator {
-        using value_type = T;
-
-        HeapAllocator() = default;
-        template <class U> constexpr HeapAllocator(const HeapAllocator<U>&) noexcept {}
-        [[nodiscard]] T* allocate(size_t n) { return static_cast<T*>(HeapAlloc(GetProcessHeap(), 0, n * sizeof(T))); }
-        void deallocate(T* p, size_t) noexcept { HeapFree(GetProcessHeap(), 0, p); }
-    };
-
-    std::vector<FrozenThread, HeapAllocator<FrozenThread>> m_frozen_threads{};
-};
+/// @brief Will modify the context of a thread's IP to point to a new address if its IP is at the old address.
+/// @param ctx The thread context to modify.
+/// @param old_ip The old IP address.
+/// @param new_ip The new IP address.
+void fix_ip(CONTEXT& ctx, uintptr_t old_ip, uintptr_t new_ip);
 } // namespace safetyhook
