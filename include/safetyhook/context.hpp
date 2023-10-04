b26234d179cadd021240d3a1873bf8a5147f229a
cursey <cursey@live.com>
2023-10-03 23:47:07 -0700

    Context: Update doc comment

--- include/safetyhook/context.hpp
+++ include/safetyhook/context.hpp
@@ -18,7 +18,7 @@ union Xmm {
 /// @brief Context structure for 64-bit MidHook.
 /// @details This structure is used to pass the context of the hooked function to the destination allowing full access
 /// to the 64-bit registers at the moment the hook is called.
-/// @note The structure only provides access to integer registers.
+/// @note rip will point to a trampoline containing the replaced instruction(s).
 struct Context64 {
     Xmm xmm0, xmm1, xmm2, xmm3, xmm4, xmm5, xmm6, xmm7, xmm8, xmm9, xmm10, xmm11, xmm12, xmm13, xmm14, xmm15;
     uintptr_t rflags, r15, r14, r13, r12, r11, r10, r9, r8, rdi, rsi, rdx, rcx, rbx, rax, rbp, rsp, rip;
@@ -27,7 +27,7 @@ struct Context64 {
 /// @brief Context structure for 32-bit MidHook.
 /// @details This structure is used to pass the context of the hooked function to the destination allowing full access
 /// to the 32-bit registers at the moment the hook is called.
-/// @note The structure only provides access to integer registers.
+/// @note eip will point to a trampoline containing the replaced instruction(s).
 struct Context32 {
     Xmm xmm0, xmm1, xmm2, xmm3, xmm4, xmm5, xmm6, xmm7;
     uintptr_t eflags, edi, esi, edx, ecx, ebx, eax, ebp, esp, eip;
