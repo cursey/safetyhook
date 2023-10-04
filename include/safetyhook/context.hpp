9aeeb28349662179e7db34afc89b9ea718e88add
cursey <cursey@live.com>
2023-10-03 19:40:22 -0700

    MidHook: Capture XMM registers

--- include/safetyhook/context.hpp
+++ include/safetyhook/context.hpp
@@ -6,11 +6,21 @@
 #include <cstdint>
 
 namespace safetyhook {
+union Xmm {
+    uint8_t u8[16];
+    uint16_t u16[8];
+    uint32_t u32[4];
+    uint64_t u64[2];
+    float f32[4];
+    double f64[2];
+};
+
 /// @brief Context structure for 64-bit MidHook.
 /// @details This structure is used to pass the context of the hooked function to the destination allowing full access
 /// to the 64-bit registers at the moment the hook is called.
 /// @note The structure only provides access to integer registers.
 struct Context64 {
+    Xmm xmm0, xmm1, xmm2, xmm3, xmm4, xmm5, xmm6, xmm7, xmm8, xmm9, xmm10, xmm11, xmm12, xmm13, xmm14, xmm15;
     uintptr_t rflags, r15, r14, r13, r12, r11, r10, r9, r8, rdi, rsi, rdx, rcx, rbx, rax, rbp, rsp, rip;
 };
 
@@ -19,6 +29,7 @@ struct Context64 {
 /// to the 32-bit registers at the moment the hook is called.
 /// @note The structure only provides access to integer registers.
 struct Context32 {
+    Xmm xmm0, xmm1, xmm2, xmm3, xmm4, xmm5, xmm6, xmm7;
     uintptr_t eflags, edi, esi, edx, ecx, ebx, eax, ebp, esp, eip;
 };
 
--- src/mid_hook.x86_64.asm
+++ src/mid_hook.x86_64.asm
@@ -19,6 +19,23 @@ push r13
 push r14
 push r15
 pushfq
+sub rsp, 256
+movdqu [rsp-240], xmm15
+movdqu [rsp-224], xmm14
+movdqu [rsp-208], xmm13
+movdqu [rsp-192], xmm12
+movdqu [rsp-176], xmm11
+movdqu [rsp-160], xmm10
+movdqu [rsp-144], xmm9
+movdqu [rsp-128], xmm8
+movdqu [rsp-112], xmm7
+movdqu [rsp-96], xmm6
+movdqu [rsp-80], xmm5
+movdqu [rsp-64], xmm4
+movdqu [rsp-48], xmm3
+movdqu [rsp-32], xmm2
+movdqu [rsp-16], xmm1
+movdqu [rsp], xmm0
 
 ; set destination parameter
 lea rcx, [rsp]
@@ -35,6 +52,23 @@ call [rel destination]
 mov rsp, rbx
 
 ; restore context
+movdqu xmm0, [rsp]
+movdqu xmm1, [rsp+16]
+movdqu xmm2, [rsp+32]
+movdqu xmm3, [rsp+48]
+movdqu xmm4, [rsp+64]
+movdqu xmm5, [rsp+80]
+movdqu xmm6, [rsp+96]
+movdqu xmm7, [rsp+112]
+movdqu xmm8, [rsp+128]
+movdqu xmm9, [rsp+144]
+movdqu xmm10, [rsp+160]
+movdqu xmm11, [rsp+176]
+movdqu xmm12, [rsp+192]
+movdqu xmm13, [rsp+208]
+movdqu xmm14, [rsp+224]
+movdqu xmm15, [rsp+240]
+add rsp, 256
 popfq
 pop r15
 pop r14
