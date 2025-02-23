e05d0de5315ba6ff0fafb8b7cb13bd7bc8baa8a0
angelfor3v3r <rainworld@riseup.net>
2025-02-22 20:30:26 -0500

    Fix optional access on linux.

--- src/os.linux.cpp
+++ src/os.linux.cpp
@@ -117,10 +117,10 @@ std::expected<VmBasicInfo, OsError> vm_query(uint8_t* address) {
             &inode, path);
 
         if (last_end < start && addr >= last_end && addr < start) {
-            info.address = reinterpret_cast<uint8_t*>(last_end);
-            info.size = start - last_end;
-            info.access = VmAccess{};
-            info.is_free = true;
+            info->address = reinterpret_cast<uint8_t*>(last_end);
+            info->size = start - last_end;
+            info->access = VmAccess{};
+            info->is_free = true;
 
             break;
         }
@@ -128,10 +128,10 @@ std::expected<VmBasicInfo, OsError> vm_query(uint8_t* address) {
         last_end = end;
 
         if (addr >= start && addr < end) {
-            info.address = reinterpret_cast<uint8_t*>(start);
-            info.size = end - start;
-            info.access = VmAccess{};
-            info.is_free = false;
+            info->address = reinterpret_cast<uint8_t*>(start);
+            info->size = end - start;
+            info->access = VmAccess{};
+            info->is_free = false;
 
             if (perms[0] == 'r') {
                 info->access.read = true;
