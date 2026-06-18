beb6fcf9cd8cef18cd2701b7702a81672c56024a
cursey <cursey@users.noreply.github.com>
2026-06-18 01:03:10 -0700

    Migrate tests to GoogleTest

--- test/main.cpp
+++ test/main.cpp
@@ -1,17 +1,6 @@
-#include <boost/ut.hpp>
+#include <gtest/gtest.h>
 
-void register_allocator_tests();
-void register_inline_hook_tests();
-void register_inline_hook_x64_tests();
-void register_mid_hook_tests();
-void register_vmt_hook_tests();
-
-int main() {
-    register_allocator_tests();
-    register_inline_hook_tests();
-    register_inline_hook_x64_tests();
-    register_mid_hook_tests();
-    register_vmt_hook_tests();
-
-    return boost::ut::cfg<>.run({.report_errors = true}) ? 1 : 0;
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
